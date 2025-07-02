ebd850340d6227a4eb8b23d073c7d7c997057d77
imthi2826 <imthiyasi778@gmail.com>
2025-07-02 14:48:15 +0530

    Add onboarding screen for user app

---
+++ lib/onboardscreen.dart
@@ -0,0 +1,55 @@
+import 'package:deliveryapp/service/widget_size.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        width:MediaQuery.of(context).size.width,
+        margin: EdgeInsets.only(top:20,bottom: 40),
+        child: Column(
+          children: [
+            Lottie.asset("assets/delivery-boy.json",
+            ),
+            SizedBox(height: 20,),
+            Text("The Fastest \n Food Delivery",
+            textAlign: TextAlign.center,
+            style: AppWidget.lineTextFieldStyle(),
+            ),
+            SizedBox(height: 20,),
+            Text("Craving something delicious? \nOrder now and get your favorites \nDelivery fast!",
+            textAlign: TextAlign.center,
+              style: AppWidget.simplelineTextFieldStyle(),
+            ),
+            Spacer(),
+
+            Container(
+              height: 70,
+              width: 250,
+              decoration: BoxDecoration(
+                color: Colors.brown,
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: TextButton(onPressed: (){},
+                  child: Text("Get Start",
+                  style: TextStyle(color: Colors.white,fontSize: 15),
+                  )),
+            )
+          ],
+
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/widget_size.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppWidget{
+
+  static TextStyle lineTextFieldStyle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 40.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle simplelineTextFieldStyle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 20.0,
+
+    );
+  }
+}
