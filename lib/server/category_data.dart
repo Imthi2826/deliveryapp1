f575596bc6b1feb443e73dd433ac93ad8bd9aef5
Mohamed Imthiyas <imthiyasi778@gmail.com>
2025-07-15 12:31:27 +0530

    Add files via upload

---
+++ lib/server/category_data.dart
@@ -0,0 +1,34 @@
+
+import 'package:deliveryapp/model/category_model.dart';
+
+List<CategoryModel> getCategories() {
+  List<CategoryModel> category = [];
+  CategoryModel categoryModel= CategoryModel(name: '', image: '');
+
+  // Category 1: Pizza
+  categoryModel.name = "Pizza";
+  categoryModel.image = "assets/pizza.jpg";
+  category.add(categoryModel);
+  categoryModel =CategoryModel(name: '', image: '');
+
+  // Category 2: Burger
+  categoryModel =CategoryModel(name: '', image: '');
+  categoryModel.name = "Burger";
+  categoryModel.image = "assets/burger.jpg";
+  category.add(categoryModel);
+
+
+  categoryModel =CategoryModel(name: '', image: '');
+  categoryModel.name = "Desserts";
+  categoryModel.image = "assets/dessert.jpg";
+  category.add(categoryModel);
+
+  //
+  categoryModel =CategoryModel(name: '', image: '');
+  categoryModel.name = "Desserts";
+  categoryModel.image = "assets/dessert.jpg";
+  category.add(categoryModel);
+
+  //
+  return category;
+}
